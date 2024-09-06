189c3efee8c8fae2fb634fe76d660e39ccc8c8e7
EyerusalemTesfaye <tesfayerusalem@gmail.com>
2024-09-06 10:15:07 +0300

    fix: log image

--- src/app/feature/admin-form/admin-form.component.ts
+++ src/app/feature/admin-form/admin-form.component.ts
@@ -1,95 +1,4 @@
-// import { Component } from '@angular/core';
-// import { MatInputModule } from '@angular/material/input';
-// import { MatFormFieldModule } from '@angular/material/form-field';
-// import { MatSelectModule } from '@angular/material/select';
-// import { MatButtonModule } from '@angular/material/button';
-// import { FormsModule } from '@angular/forms';
-// import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
-// import { ActivatedRoute, Router } from '@angular/router';
-// import { CrudService } from '../../core/crud.service';
-// import { CommonModule } from '@angular/common';
-// import { data } from 'autoprefixer';
 
-// @Component({
-//   selector: 'app-admin-form',
-//   standalone: true,
-//   imports: [ MatInputModule,
-//     MatFormFieldModule,
-//     MatSelectModule,
-//     MatButtonModule,
-//     ReactiveFormsModule,
-//     CommonModule,
-//     FormsModule],
-//   templateUrl: './admin-form.component.html',
-//   styleUrl: './admin-form.component.css'
-// })
-// export class AdminFormComponent {
-//   adminForm: FormGroup;
-//   isEdit: boolean = false;
-//   isViewDetail:boolean = false;
-//   mode!: string
-//   id!:number
-//   constructor(private fb: FormBuilder, private route: ActivatedRoute, private router:Router, private crudservice:CrudService<any>) {
-//     this.adminForm = this.fb.group({
-//       fullName: ['', Validators.required],
-//       email: ['', [Validators.required, Validators.email]],
-//       phoneNumber: ['', Validators.required],
-//       country: ['', Validators.required],
-//       city: ['', Validators.required],
-//       user_group_id: ['', Validators.required],
-//       status: ['', Validators.required],
-//       password: ['', Validators.required]
-//     });
-
-//     this.isViewDetail  = this.router.getCurrentNavigation()?.extras.state?.['view'];
-//     //this.isEdit  = this.router.getCurrentNavigation()?.extras.state?.['edit'];
-
-//   }
-//   ngOnInit() {
-
-//     this.route.paramMap.subscribe(params => {
-//        this.id = Number(params.get('id'));
-//       this.crudservice.getById('admin/getadminbyid', this.id).subscribe(
-//         (data)=>{
-//           this.adminForm.patchValue({
-//             fullName: data.fullName,
-//             email: data.email,
-//             phoneNumber: data.phoneNumber,
-//             country: data.country,
-//             city: data.city,
-//             user_group_id: data.user_group_id,
-//             status: data.isActive ? 'Active' : 'Inactive'
-//           });
-//         }
-//       )
-//       this.crudservice.getAll('userGroup/getAllUserGroups').subscribe((res:any) => {
-//         console.log('user group data', res.data);
-//       })
-//       this.route.queryParamMap.subscribe(params => {
-//         this.isViewDetail = params.get('view') === 'true';
-//         this.isEdit = params.get('edit') === 'true';
-//       if (this.isViewDetail) {
-//         this.adminForm.disable();
-//       }
-//     });
-//     });
-
-//   }
-//   onSave() {
-//     if (this.adminForm.valid) {
-//      this.crudservice.create('admin/createadmins', this.adminForm.value).subscribe(respose =>{
-//        console.log('admin create', respose);
-//      })
-//      //this.router.navigate(['/admins'])
-//     } else {
-//       console.log('Form not valid!');
-//     }
-//   }
-
-//   onEdit() {
-//     console.log('Edit button clicked');
-//   }
-// }
 import { Component, OnInit } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -178,22 +87,6 @@ export class AdminFormComponent implements OnInit {
     });
   }
 
-  // onSave() {
-  // console.log('this+++++', this.id)
-  //   if (this.adminForm.valid) {
-
-  //     this.crudservice.create('admin/createadmins', this.adminForm.value).subscribe((response) => {
-  //       console.log('admin create', response);
-  //       this.snackBar.open('admin created successfully!', 'Close', {
-  //         duration: 3000,
-  //          verticalPosition: 'top'
-  //       });
-  //       this.router.navigate(['/admins'])
-  //     });
-  //   } else {
-  //     console.log('Form not valid!');
-  //   }
-  // }
   onSave() {
     if (this.adminForm.valid) {
       if (this.id) {
